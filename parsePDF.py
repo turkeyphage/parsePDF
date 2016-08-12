dbc1200fc435ed8d4964d69328dcdcf473e0f622
Roger Lo <turkeyworld64@gmail.com>
2016-08-12 16:23:27 +0800

    Modified parsePDF.py to read text file directly. Command: python3 parsePDF.py /text/file/path/filename.txt

--- parsePDF.py
+++ parsePDF.py
@@ -1,67 +1,62 @@
-try:  
-    from cStringIO import StringIO  
-except ImportError:  
-    from StringIO import StringIO
 
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
+#try:  
+#    from cStringIO import StringIO  
+#except ImportError:  
+#    from StringIO import StringIO
+
+#from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+#from pdfminer.converter import TextConverter
+#from pdfminer.layout import LAParams
+#from pdfminer.pdfpage import PDFPage
 import os, glob, re, sys
 
-from pdfminer.pdfparser import PDFParser
-from pdfminer.pdfdocument import PDFDocument
+#from pdfminer.pdfparser import PDFParser
+#from pdfminer.pdfdocument import PDFDocument
 
 from collections import Counter
 
 # change to assigned folder and list all PDF filenames
-def get_All_PDF_Name(directoryPath):
+def get_All_TXT_Name(directoryPath):
     os.chdir(directoryPath)
-    allPDF = []
-    for filename in glob.glob("*.pdf"):
-	allPDF.append(filename)
-    return allPDF
-
+    allTXT = []
+    for filename in glob.glob("*.txt"):
+        allTXT.append(filename)
+    return allTXT
 
 #get PDF's metadata
-def getPDF_Metadata(filename):
-    print('filename = ' + filename)
-    fp = open(filename, 'rb')
-    parser = PDFParser(fp)
-    doc = PDFDocument(parser)
-    return(doc.info)
+#def getPDF_Metadata(filename):
+#    print('filename = ' + filename)
+#    fp = open(filename, 'rb')
+#    parser = PDFParser(fp)
+#    doc = PDFDocument(parser)
+#    return(doc.info)
 
 
 # get content of PDF and convert to txt string
 # how to use it? ex. convert('myfile.pdf', pages=[5,7])
 
-def convert(fname, pages=None):
-    if not pages:
-        pagenums = set()
-    else:
-        pagenums = set(pages)
+#def convert(fname, pages=None):
+#    if not pages:
+#        pagenums = set()
+#    else:
+#        pagenums = set(pages)
+#
+#    output = StringIO()
+#    manager = PDFResourceManager()
+#    converter = TextConverter(manager, output, laparams=LAParams())
+#    interpreter = PDFPageInterpreter(manager, converter)
+#
+#    infile = file(fname, 'rb')
+#    for page in PDFPage.get_pages(infile, pagenums):
+#        interpreter.process_page(page)
+#    infile.close()
+#    converter.close()
+#    text = output.getvalue()
+#    output.close
+#    return text
 
-    output = StringIO()
-    manager = PDFResourceManager()
-    converter = TextConverter(manager, output, laparams=LAParams())
-    interpreter = PDFPageInterpreter(manager, converter)
-
-    infile = file(fname, 'rb')
-    for page in PDFPage.get_pages(infile, pagenums):
-        interpreter.process_page(page)
-    infile.close()
-    converter.close()
-    text = output.getvalue()
-    output.close
-    return text
 
 
-# modify string
-'''
-def removeEmptyLine(txt):
-    return re.sub("\n\s*\n*", "\n", txt)
-
-'''
 
 def count_word_occurrences(list_of_words):
     return Counter(list_of_words)
@@ -75,6 +70,7 @@ def split_string_with_re(txt):
 
 # main program 
 
+##### 1. read file.txt to string variable
 '''
 if len(sys.argv) < 2:
     #no argument
@@ -91,7 +87,6 @@ elif len(sys.argv) > 2:
 filenameList = get_All_PDF_Name(sys.argv[1])
 
 print(filenameList)
-
 '''
 
 if len(sys.argv) < 2:
@@ -108,16 +103,22 @@ head, tail = os.path.split(sys.argv[1])
 print('folder: '+ head)
 print('filename: '+ tail)
 os.chdir(head)
-fullText = convert(tail, 0)
+#fullText = convert(tail, 0)
 
+#read file
+f = open(tail, 'rt')
+fullText = f.read()
+f.close
+
+#count word occurrences
 reResult = count_word_occurrences(split_string_with_re(fullText))
 
-for key in reResult.keys():
-    exceptList = ['is','are','the','The','this','This','that','That','V','not','Not','yes','no','Yes','No','v','and','And','to','To','pages','of','before','over','in','In','on','On','then','between','after','pages','with','for','all','by','By','table','Table','Figure','figure','can','from','data','or','if','else','its','than','be','only','out','Out','datasheet']
+exceptList = ['is','are','the','The','this','This','that','That','V','not','Not','yes','no','Yes','No','v','and','And','to','To','pages','of','before','over','in','In','on','On','then','between','after','pages','with','for','all','by','By','table','Table','Figure','figure','can','from','data','or','if','else','its','than','be','only','out','Out','datasheet']
 
-    for item in exceptList:
-        if key == item:
-	   del reResult[key]
+
+for item in exceptList:
+    if item in reResult:
+       del reResult[item]
 
 print(reResult)
 
