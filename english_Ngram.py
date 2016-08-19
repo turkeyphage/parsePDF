e4eee7104e6909566704d5545dfaf38fef875914
Roger Lo <turkeyworld64@gmail.com>
2016-08-19 18:15:02 +0800

    modified english_Ngram.py fot testing

--- english_Ngram.py
+++ english_Ngram.py
@@ -13,8 +13,24 @@ after_ngram = ngram_String(txt, n)
 
 print(type(after_ngram))
 
+
+all_list = []
+for item in after_ngram:
+  #print(len(item)
+  s =''
+  for index in range(len(item)):
+    #print(item[index])
+    if index == (len(item)-1):
+       s += item[index] 
+    else:
+       s += item[index]+' '
+  #print(s)
+  all_list.append(s)
+print(all_list)
+'''
 for grams in after_ngram:
     #print(grams)
     #combine the words
     s = grams[0]+' '+grams[1]+' '+grams[2]
     print(s)
+'''
