86b484dd3eb56b731cfbb780f7d42a4c64bd52d7
roger <turkeyworld64@gmail.com>
2016-08-25 16:00:39 +0800

    modify parsePDF.py exceptList and add dfilter_fuc into it

--- parsePDF.py
+++ parsePDF.py
@@ -1,23 +1,8 @@
-
-#try:  
-#    from cStringIO import StringIO  
-#except ImportError:  
-#    from StringIO import StringIO
-
-#from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-#from pdfminer.converter import TextConverter
-#from pdfminer.layout import LAParams
-#from pdfminer.pdfpage import PDFPage
-
 import os, glob, re, sys, operator
+
 from english_Ngram import ngram_process
-
-
-#from pdfminer.pdfparser import PDFParser
-#from pdfminer.pdfdocument import PDFDocument
-
 from collections import Counter
-
+from dfilter_fuc import dfilter_fuc
 
 ngram_number = 2
 
@@ -28,39 +13,6 @@ def get_All_TXT_Name(directoryPath):
     for filename in glob.glob("*.txt"):
         allTXT.append(filename)
     return allTXT
-
-#get PDF's metadata
-#def getPDF_Metadata(filename):
-#    print('filename = ' + filename)
-#    fp = open(filename, 'rb')
-#    parser = PDFParser(fp)
-#    doc = PDFDocument(parser)
-#    return(doc.info)
-
-
-# get content of PDF and convert to txt string
-# how to use it? ex. convert('myfile.pdf', pages=[5,7])
-
-#def convert(fname, pages=None):
-#    if not pages:
-#        pagenums = set()
-#    else:
-#        pagenums = set(pages)
-#
-#    output = StringIO()
-#    manager = PDFResourceManager()
-#    converter = TextConverter(manager, output, laparams=LAParams())
-#    interpreter = PDFPageInterpreter(manager, converter)
-#
-#    infile = file(fname, 'rb')
-#    for page in PDFPage.get_pages(infile, pagenums):
-#        interpreter.process_page(page)
-#    infile.close()
-#    converter.close()
-#    text = output.getvalue()
-#    output.close
-#    return text
-
 
 
 
@@ -74,26 +26,10 @@ def split_string_with_re(txt):
     allresult = re.findall(pattern1, txt)
     return allresult
 
-# main program 
+
+####### main program ##########
 
 ##### 1. read file.txt to string variable
-'''
-if len(sys.argv) < 2:
-    #no argument
-    print('please assign the pdf_folder path and try again. ex: python parsePDF.py /pdf/path/')
-    sys.exit()
-elif len(sys.argv) > 2:
-    print('too many argv, please try again')
-    sys.exit()
-
-#print(sys.argv[0])
-#print(sys.argv[1])
-#print(get_All_PDF_Name(sys.argv[1]))
-
-filenameList = get_All_PDF_Name(sys.argv[1])
-
-print(filenameList)
-'''
 
 if len(sys.argv) < 2:
     #no argument
@@ -109,7 +45,6 @@ head, tail = os.path.split(sys.argv[1])
 print('folder: '+ head)
 print('filename: '+ tail)
 os.chdir(head)
-#fullText = convert(tail, 0)
 
 #read file
 f = open(tail, 'rt')
@@ -117,25 +52,24 @@ fullText = f.read()
 f.close
 
 
-#filted_words is a list
-filtered_words = split_string_with_re(fullText)
+#type(filted_words) = List
+filtered_words = split_string_with_re(fullText.lower())
 
-
-#test n-gram
-
-print('n_gram = ' , ngram_number)
-print('n_gram result:\n')
+#type(filteredwords2String) = String
 filteredwords2String = ' '.join(filtered_words)
+
+
+#process n-gram testing:
+print('n_gram = ', ngram_number)
+print('n_gram result:\n')
 n_gram_result = ngram_process(filteredwords2String, ngram_number)
+#type(n_gram_result) = Dictionary
 #print(n_gram_result)
 
 sorted_ngram = sorted(n_gram_result.items(), key=operator.itemgetter(1))
 print(sorted_ngram)
 
-
-
 print('==================================================')
-
 
 
 #print(type(filtered_words))
@@ -144,7 +78,7 @@ print('==================================================')
 #count word occurrences
 reResult = count_word_occurrences(filtered_words)
 
-exceptList = ['was','Was','Were','were','is','Is','Are','are','the','The','this','This','that','That','not','Not','yes','no','Yes','No','and','And','to','To','Page','page','pages','Pages','of','Of','Before','before','over','Over','in','In','on','On','then','Then','between','Between','after','After','with','With','for','For','all','All','by','By','table','Table','Figure','figure','can','Can','from','From','data','Data','Or','or','if','If','else','Else','its','Its','than','Than','be','Be','only','Only','out','Out','datasheet','Datasheet']
+exceptList = ['was','were','is','are','not','yes','no','and','to','page','pages','before','over','in','on','then','between','after','with','for','by','table','figure','can','could','from','data','or','if','else','than','be','only','out','datasheet','the','a','an','this','that','these','those','my','yours','his','her','its','our','their','few','little','much','many','lot','lots','of','most','some','any','enough','all','both','half','either','neither','each','every','other','another','such','what','who','where','how','whom','which','will','rather','quite','over', 'do','did', 'does','you','we','it','like','want','they']
 
 
 for item in exceptList:
@@ -159,7 +93,6 @@ print(reResult)
 #print(reResult['High'])
 
 '''
-
 for eachFile in filenameList:
     pdfMetadata = getPDF_Metadata(eachFile)
     print(pdfMetadata[0].items())
@@ -167,9 +100,6 @@ for eachFile in filenameList:
 
 '''
 
-#print(type(meta))
-#print(len(meta))
-#print(type(meta[0]))
 
 
 
