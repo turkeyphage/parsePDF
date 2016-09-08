ff8ee0ba069a6219be1791fbb8b80697a35427d2
roger <turkeyworld64@gmail.com>
2016-09-08 17:51:33 +0800

    modify parsePDF.py for getting manufacturer result

--- parsePDF.py
+++ parsePDF.py
@@ -4,6 +4,9 @@ from english_Ngram import ngram_process
 from collections import Counter
 from dfilter_fuc import dfilter_fuc
 
+
+manufacturer_search_result=[]
+
 ngram_number = 2
 
 # change to assigned folder and list all PDF filenames
@@ -15,16 +18,36 @@ def get_All_TXT_Name(directoryPath):
     return allTXT
 
 
-
 def count_word_occurrences(list_of_words):
     return Counter(list_of_words)
 
 
 def split_string_with_re(txt):
     #pattern1 is select all words except "\n" "\s" "\t" ","
-    pattern1 = re.compile(r'[\w]+\.?[\w]+\.?')
+    pattern1 = re.compile(r'[\w]+[=]?[\w]+[°C]?\.?[\w]+\.?[\w]+')
     allresult = re.findall(pattern1, txt)
+    #for item in allresult:
+       #print(item)
     return allresult
+
+
+def compare_with_target_list(compare_file_name, target):
+    result=[]
+
+    #target_in_string = ' '.join(target)
+    #print(target_in_string)
+    #target_in_string_lower = target_in_string.lower()
+    #print(target_in_string.lower())
+    with open(compare_file_name) as f:
+       compare_items_list = f.read().splitlines() 
+ 
+    for eachItem in compare_items_list:
+       if eachItem.lower() in [x.lower() for x in target]:
+            result.append(eachItem)
+       #if eachItem.lower() in target_in_string_lower:
+       #     result.append(eachItem)
+
+    return result
 
 
 ####### main program ##########
@@ -36,9 +59,7 @@ if len(sys.argv) < 2:
     print('please assign some arguments and try again. ex: python3 parsePDF.py filename.txt')
     exit(1)
 
-
-
-print('filename: ' + sys.argv[1])
+#print('filename: ' + sys.argv[1])
 
 #read file
 
@@ -46,22 +67,31 @@ with open(sys.argv[1]) as f:
      original_texts = f.read()
 
 
-fullText = re.sub("[(),=\"”“/]", "", original_texts)
+fullText = re.sub("[(),\"”“/]", " ", original_texts)
+
 fullText = re.sub("[-―･\n–&:]"," ", fullText)
-
-#print(fullText)
-    
-
 
 
 
 #type(filted_words) = List
-filtered_words = split_string_with_re(fullText.lower())
+filtered_words = split_string_with_re(fullText)
+
+
+
+#Compare with Manufacturers List
+#print('Searching Manufacturer List:')
+manufacturer_search_result += compare_with_target_list('manufacturer_list.txt', filtered_words)
+#print(manufacturer_search_result) if len(manufacturer_search_result)!= 0 else print('no found') 
+
+
+
+
+
 
 #type(filteredwords2String) = String
 filteredwords2String = ' '.join(filtered_words)
 
-print(filteredwords2String)
+#print(filteredwords2String)
 #process n-gram testing:
 
 #print('n_gram = ', ngram_number)
@@ -69,12 +99,18 @@ print(filteredwords2String)
 
 n_gram_result = ngram_process(filteredwords2String, ngram_number)
 #type(n_gram_result) = Dictionary
-#print(n_gram_result)
+#print(n_gram_result.keys())
+
+print('Searching Manufacturer List: ')
+manufacturer_search_result += compare_with_target_list('manufacturer_list.txt', n_gram_result.keys())
+print(manufacturer_search_result) if len(manufacturer_search_result)!= 0 else print('no found')
+
+
 
 sorted_ngram = sorted(n_gram_result.items(), key=operator.itemgetter(1))
 #print(sorted_ngram)
 
-print('==================================================')
+#print('==================================================')
 
 
 #print(type(filtered_words))
